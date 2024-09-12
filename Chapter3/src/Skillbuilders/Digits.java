5ea86c3b25174e4aef7acddcca3ce429d376cefc
77198001 <77198001@L815WD220030.edu.cbe.ab.ca>
2024-09-12 09:58:25 -0600

    10:00 AM

--- Chapter3/src/Skillbuilders/Digits.java
+++ Chapter3/src/Skillbuilders/Digits.java
@@ -6,7 +6,7 @@ public class Digits {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Please enter a two-digit number: ");
+        System.out.print("Please enter your class grades: ");
         int number = scanner.nextInt();
 
         int tens = number / 10;
---
+++ Chapter3/src/Skillbuilders/GradeAvg1.java
@@ -0,0 +1,21 @@
+package Skillbuilders;
+
+import java.util.Scanner;
+
+public class GradeAvg1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your first grade:");
+		int g1 = scanner.nextInt();
+		System.out.print("Please enter your second grade:");
+		int g2 = scanner.nextInt();
+		System.out.print("Please enter your third grade:");
+		int g3 = scanner.nextInt();
+		System.out.print("Please enter your fourth grade:");
+		int g4 = scanner.nextInt();
+		System.out.print("Please enter your fifth grade:");
+		int g5 = scanner.nextInt();
+		int avg = (g1 + g2 + g3 + g4 + g5)/5;
+		System.out.print("Your grade average is:"+ Math.round(avg));
+    }
+}
