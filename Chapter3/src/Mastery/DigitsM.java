906a89826f6b175297cfa76d48c500759feb825a
77198001 <77198001@L815WD220030.edu.cbe.ab.ca>
2024-09-13 10:28:37 -0600

    10:29 AM

---
+++ Chapter3/src/Mastery/DigitsM.java
@@ -0,0 +1,41 @@
+/*
+Program: java          Date: 9/13/2024
+
+Purpose: Do find the ones, tens, and hundreds place of a given number
+
+Author: Malik Muhieddine
+School: CHHS
+Course: Computer Science 20
+ 
+*/
+package Mastery;
+
+import java.util.Scanner;
+
+public class DigitsM {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter your number: ");
+        
+        int number = scanner.nextInt();
+
+        int ones = (number/1)%10;
+
+        int tens = (number/10)%10;
+        
+        int hund = (number/100)%10;
+
+        System.out.println("The Ones place digit is: " + ones);
+        System.out.println("The Tens place digit is: " + tens);
+        System.out.println("The Hundreds place digit is: " + hund);
+    }
+}
+
+/*
+Screen Dump
+Please enter your number: 151
+The Ones place digit is: 1
+The Tens place digit is: 5
+TheHundreds place digit is: 1
+*/
